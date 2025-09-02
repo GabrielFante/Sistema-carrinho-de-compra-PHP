1096a6de63804cebf06c924c95df910c3cd3df7a
João Pedro Pereira Guerra <117650440+SmotzClean@users.noreply.github.com>
2025-09-01 21:42:19 -0300

    Add files via upload

---
+++ Carrinho.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once 'Product.php';
+
+class Cart
+{
+    private const DISCOUNT10 = 0.10;
+    private array $cart = [];
+
+    public function addProduct(Product $product) {
+        
+    }
+
+    public function removeProduct(Product $product) {
+        
+    }
+
+    public function listCart() {
+        
+    }
+
+    public function calculateTotal() {
+        
+    }
+
+    public function addDiscount() {
+
+    }
+
+}
+
+?>
