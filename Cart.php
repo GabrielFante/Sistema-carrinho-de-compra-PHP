0f0789d01671cfd62b362a9c9dfa3843a8ddd8b4
João Pedro Pereira Guerra <117650440+SmotzClean@users.noreply.github.com>
2025-09-04 16:40:24 -0300

    Update Guerra Cart.php
    adicionei função findItens e comecei o additens

--- Cart.php
+++ Cart.php
@@ -7,26 +7,48 @@ class Cart
     private const DISCOUNT10 = 0.10;
     private array $cart = [];
 
-    public function addProduct(Product $product) {
-        
+    //Cria função de buscar item
+    private function findItem(Product $product): ?int
+    {
+        foreach ($this->cart as $index => $cartItem) {
+            if($cartItem['product']->getId() === $product->getid()){
+                return $index;
+            }
+        }
+        return null;
     }
 
+    //adicionar produto no carrinho
+    public function addProduct(Product $product) 
+    {
+        if ($product->getStock() < $quantity){
+            echo "Estoque insuficiente do produto {$product->getNome()}";
+            return;
+        }
+
+        $index = $this->findItem($product);
+
+    }
+
+    //remover produto do carrinho
     public function removeProduct(Product $product) {
         
     }
 
+    //listar itens de dentro do carrinho
     public function listCart() {
         
     }
 
+    //Calcular o valor total do carrinho
     public function calculateTotal() {
         
     }
 
+    //adicionar o cupom de desconto ao total do carrinho
     public function addDiscount() {
 
     }
 
 }
-
 ?>
