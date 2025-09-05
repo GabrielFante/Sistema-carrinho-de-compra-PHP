88b90fa1e796eb12fcca9332a15772e11b9fda76
GabrielFante <164691063+GabrielFante@users.noreply.github.com>
2025-09-04 22:37:57 -0300

    Update Product.php

--- Product.php
+++ Product.php
@@ -8,54 +8,54 @@ class Product
     private int $stock;
 
     public function __construct(int $id, string $name, float $price, int $stock) {
+        $this->setId($id);
+        $this->setName($name);
+        $this->setPrice($price);
+        $this->setStock($stock);
+    }
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function setId(int $id): void {
+        if ($id <= 0) {
+            throw new InvalidArgumentException("Id deve ser positivo!");
+        }
         $this->id = $id;
-        $this->name = $name;
-        $this->price = $price;
-        $this->stock = $stock;
     }
 
-    public function getId(): void {
-        $this->id = $id;
+    public function getName(): string {
+        return $this->name;
     }
 
-    public function setId(int $id): int{
-        if ($id < 0 or !$id) {
-            return "Id deve ser positivo!";
+    public function setName(string $name): void {
+        if (strlen($name) < 2) {
+            throw new InvalidArgumentException("O nome deve ter pelo menos 2 caracteres");
         }
-        return $id;
-    }
-
-    public function getName(): void {
         $this->name = $name;
     }
 
-    public function setName(string $name): string {
-        if (!$name or $name < 2) {
-            return "O nome deve ser maior que 2 digitos";
-        }
-        return $name;
+    public function getPrice(): float {
+        return $this->price;
     }
 
-    public function getPrice(): void{
+    public function setPrice(float $price): void {
+        if ($price <= 0) {
+            throw new InvalidArgumentException("O preço deve ser maior que 0");
+        }
         $this->price = $price;
     }
 
-    public function setPrice(float $price): float {
-        if (!$price or $price < 0) {
-            return "O preço deve ser maior do que 0";
-        }
-        return $price;
+    public function getStock(): int {
+        return $this->stock;
     }
 
-    public function getStock(): void {
+    public function setStock(int $stock): void {
+        if ($stock < 0) {
+            throw new InvalidArgumentException("O estoque deve ser maior ou igual a 0");
+        }
         $this->stock = $stock;
-    }
-
-    public function setStock(int $stock): int {
-        if (!$stock or $stock < 0) {
-            return "O Estoque deve ser maior que 0";
-        }
-        return $stock;
     }
 }
 ?>
